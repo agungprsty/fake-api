6200d0fc75ef8d6fd6365a95a9f6014bec8b5a89
agung <agungprsty423@gmail.com>
2022-08-27 12:27:56 +0700

    add homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Free fake REST API for testing and prototyping."/>
+    <title>JSONFaker - Free Fake REST API</title>
+
+    <!--Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Bootstrap CSS / Color Scheme -->
+    <link rel="stylesheet" href="{{ url("assets/css/bootstrap.css") }}">
+</head>
+<body>
+
+    <!--Header components-->
+    @include('components.header')
+
+    <!--Section components-->
+    @include('components.section')
+
+    <!--Footer components-->
+    @include('components.footer')
+
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.7.3/feather.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/prism.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/plugins/line-numbers/prism-line-numbers.min.js"></script>
+<script src="{{ url("assets/js/scripts.js") }}"></script>
+@stack('script')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->get('/', function () {
+    return view('home');
 });
