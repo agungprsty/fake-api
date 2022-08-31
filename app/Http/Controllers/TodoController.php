4534c41f191c6657de30e1e0dd99d7e17e531e52
agung <agungprsty423@gmail.com>
2022-08-31 23:25:02 +0700

    add documentasi todos

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -45,6 +45,37 @@ class TodoController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
+
+    /**
+     * @OA\Get(
+     *     tags={"Todo"},
+     *     path="/api/todos",
+     *     description="List of todo",
+     *     @OA\Response(response="200", description="OK",
+     *     content={
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="data",
+     *                     type="array",
+     *                     description="List of todo",
+     *                     @OA\Items(type="object"),
+     *                     example={
+     *                      {
+     *                          "uid": 1,
+     *                          "id": 1,
+     *                          "title": "delectus aut autem",
+     *                          "completed": false,
+     *                      }
+     *                    } 
+     *                 )
+     *             )
+     *         )
+     *     }
+     *  )
+     * )
+     */
     public function all()
     {
         try {
@@ -60,6 +91,45 @@ class TodoController extends Controller
      * Get by id todo.
      *
      * @return \Illuminate\Http\JsonResponse
+     */
+
+    /**
+     * @OA\Get(
+     *     tags={"Todo"},
+     *     path="/api/todos/{id}",
+     *     description="Get by id todo",
+     *     @OA\Parameter(
+     *         name="id",
+     *         description="Todo ID",
+     *         example = 1,
+     *          in = "path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer"
+     *         ) 
+     *     ),
+     *     @OA\Response(response="200", 
+     *      description="OK",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="data",
+     *                         type="object",
+     *                         description="Find todo by id"
+     *                     ),
+     *                     example={
+     *                          "uid": 1,
+     *                          "id": 1,
+     *                          "title": "quis ut nam facilis et officia qui",
+     *                          "completed": false,
+     *                     }
+     *                 )
+     *             )
+     *         }
+     *      ),
+     *  )
      */
     public function get_by_id(string $id)
     {
@@ -82,6 +152,51 @@ class TodoController extends Controller
      * @param  TodoRequest  $request
      * @return \Illuminate\Http\JsonResponse
      */
+
+    /**
+     * @OA\Post(
+     *     tags={"Todo"},
+     *     path="/api/todos",
+     *     description="Create new todo",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="title",
+     *                     type="string",
+     *                     example="Title hello world!"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="completed",
+     *                     type="boolean",
+     *                     example=false
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", 
+     *      description="OK",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="data",
+     *                         type="object",
+     *                         description="Create new user"
+     *                     ),
+     *                     example={
+     *                          "id": 201,
+     *                          "title": "Title hello world!",
+     *                          "completed": false,
+     *                     }
+     *                 )
+     *             )
+     *         }
+     *      ),
+     *  )
+     */
     public function store(TodoRequest $request)
     {
         try {
@@ -98,6 +213,62 @@ class TodoController extends Controller
      *
      * @param  TodoRequest  $request
      * @return \Illuminate\Http\JsonResponse
+     */
+
+    /**
+     * @OA\Put(
+     *     tags={"Todo"},
+     *     path="/api/todos/{id}",
+     *     description="Update a todo",
+     *     @OA\Parameter(
+     *         name="id",
+     *         description="Todo ID",
+     *         example = 1,
+     *          in = "path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer"
+     *         ) 
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="title",
+     *                     type="string",
+     *                     example="Update title hello world!"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="completed",
+     *                     type="boolean",
+     *                     example=true
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", 
+     *      description="OK",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="data",
+     *                         type="object",
+     *                         description="Update a todo"
+     *                     ),
+     *                     example={
+     *                          "uid": 1,
+     *                          "id": 1,
+     *                          "title": "Update Title hello world!",
+     *                          "completed": false,
+     *                     }
+     *                 )
+     *             )
+     *         }
+     *      ),
+     *  )
      */
     public function update(string $id, TodoRequest $request)
     {
@@ -119,6 +290,26 @@ class TodoController extends Controller
      * Delete todo
      *
      * @return \Illuminate\Http\JsonResponse
+     */
+
+    /**
+     * @OA\Delete(
+     *     tags={"Todo"},
+     *     path="/api/todos/{id}",
+     *     description="Delete a todo",
+     *     @OA\Parameter(
+     *         name="id",
+     *         description="Todo ID",
+     *         example = 1,
+     *          in = "path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer"
+     *         ) 
+     *     ),
+     *     @OA\Response(response="200", 
+     *      description="OK"),
+     *  )
      */
     public function delete(string $id)
     {
