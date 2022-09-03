91235a7350fc3a2385febe302e12706b1e0b6e24
agung <agungprsty423@gmail.com>
2022-09-03 19:05:45 +0700

    add helpers function

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Http\Response;
+
+
 if(!function_exists('safe_int')){
     /**
      * Safely convert a string to an integer.
@@ -10,5 +13,53 @@ if(!function_exists('safe_int')){
     }
 }
 
+if(!function_exists('json_response')){
+    /**
+     * Wrap json response.
+     */
+    function json_response(
+        mixed $data = null,
+        mixed $pagination = null,
+        int $status = 200
+    ){
+        $content = [
+            'meta' => [
+                'status' => $status,
+                'message' => Response::$statusTexts[$status]
+            ],
+        ];
 
+        if ($data){
+            $content['data'] = $data;
+        }
+        if ($pagination){
+            $content['meta']['pagination'] = $pagination;
+        }
 
+        return response()->json($content, $status);
+    }
+}
+
+if(!function_exists('check_query_string')){
+    /**
+     * Check query string.
+     */
+    function check_query_string(): array
+    {
+        $result['page'] = isset($_GET['page']) ? intval($_GET['page']) : 1;
+        $result['per_page'] = isset($_GET['per_page']) ? intval($_GET['per_page']) : 10;
+
+        // Validation: Page to display can not be less than 1 or 
+        // Request page greater than 100
+        if ($result['page'] < 1 || $result['page'] > 100) {
+            $result['page'] = 1;
+        }
+
+        // Validation: Request per page greater than 100
+        if ($result['per_page'] > 100) {
+            $result['per_page'] = 10;
+        }
+
+        return $result;
+    }
+}
