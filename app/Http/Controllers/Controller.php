874ce4eaa178bf21a541c8c1ea4a95adcddfd14b
agung <agungprsty423@gmail.com>
2022-09-03 17:49:43 +0700

    hotfix apispec schemaAuth

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -23,8 +23,6 @@ use Laravel\Lumen\Routing\Controller as BaseController;
  * @OA\SecurityScheme(
  *     type="http",
  *     description="Login with email and password to get the authentication token",
- *     name="Token based Based",
- *     in="header",
  *     scheme="bearer",
  *     bearerFormat="JWT",
  *     securityScheme="bearerAuth",
